9462114c67112e18ea086aa69322bc94ea90ed84
Jeroen Dalsem <jdalsem@coldtrick.com>
2015-06-12 13:54:20 +0200

    fixed: incorrect buttons for single step wizard

--- views/default/wizard/pagination.php
+++ views/default/wizard/pagination.php
@@ -6,27 +6,26 @@ $steps = (array) elgg_extract('steps', $vars);
 $count = count($steps);
 
 echo '<div ' . elgg_format_attributes(array('class' => 'elgg-foot')) . '>';
-if ($count) {
-	if ($count > 1) {
-		echo '<ul class="elgg-pagination float man">';
-		foreach ($steps as $index => $step) {
-			$attrs = array(
-				'data-step' => $index
-			);
-			
-			if ($index === 0) {
-				$attrs['class'] = 'elgg-state-selected';
-			} else {
-				$attrs['class'] = 'elgg-state-disabled';
-			}
-			
-			echo '<li ' . elgg_format_attributes($attrs) . '>';
-			echo '<a href="#" onclick="elgg.wizard.step(' . $index . ')">' . ($index + 1) . '</a>';
-			echo '<span>' . ($index + 1) . '</span>';
-			echo '</li>';
+if ($count > 1) {
+	
+	echo '<ul class="elgg-pagination float man">';
+	foreach ($steps as $index => $step) {
+		$attrs = array(
+			'data-step' => $index
+		);
+		
+		if ($index === 0) {
+			$attrs['class'] = 'elgg-state-selected';
+		} else {
+			$attrs['class'] = 'elgg-state-disabled';
 		}
-		echo '</ul>';
+		
+		echo '<li ' . elgg_format_attributes($attrs) . '>';
+		echo '<a href="#" onclick="elgg.wizard.step(' . $index . ')">' . ($index + 1) . '</a>';
+		echo '<span>' . ($index + 1) . '</span>';
+		echo '</li>';
 	}
+	echo '</ul>';
 	
 	echo elgg_view('input/button', array(
 		'value' => elgg_echo('next'),
